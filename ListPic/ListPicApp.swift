a5ac0dfb9154375f24fa656dcbd60539215f8cc8
Tom <chings228@gmail.com>
2023-08-13 17:01:28 +0800

    Initial Commit

---
+++ ListPic/ListPicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListPicApp.swift
+//  ListPic
+//
+//  Created by man ching chan on 13/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct ListPicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
